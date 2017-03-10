ae796fce2e858e90a48a10b7669f2c57c8a8f160
Jue <me@jue.yt>
2017-03-10 21:35:25 +0100

    ping: Make sure the correct version of ping is installed

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,11 +1,37 @@
 'use strict';
 
 const spawn = require( 'child_process' ).spawn;
+const exec = require( 'child_process' ).exec;
 
 const REaddr = /((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}/;
 const REping = /^\[([0-9.]*)\].*time=([0-9.]+) ms$/;
 
 module.exports = function( config, mqtt ) {
+
+	// Some defaults
+	if( typeof config != 'object' ) config = {};
+	if( typeof config.interval != 'number' ) config.interval = 2000;
+	config.interval /= 1000;
+
+	// Check if correct ping tool is installed
+	if( config.hosts instanceof Array ) exec( 'ping -V', ( err, stdout, stderr ) => {
+		if( err || stdout.indexOf( 'iputils' ) == -1 ) {
+			console.log( "Ping of iputils must be installed" );
+			return;
+		}
+
+		// Kickoff ping
+		for( let host of config.hosts ) {
+			if( ! REaddr.test( host ) ) {
+				console.error( `${host} is no IPv4` );
+				continue;
+			}
+			console.log( "Start publishing ping to " + host );
+			setImmediate( () => pubPing( host ) );
+		}
+	} );
+
+
 
 	let pingClosed = [];
 	let pingKill = [];
@@ -34,21 +60,6 @@ module.exports = function( config, mqtt ) {
 		} );
 
 		pingProc.stderr.pipe( process.stderr );
-	}
-
-
-	// Some defaults
-	if( typeof config != 'object' ) config = {};
-	if( typeof config.interval != 'number' ) config.interval = 2000;
-	config.interval /= 1000;
-
-	if( config.hosts instanceof Array ) for( let host of config.hosts ) {
-		if( ! REaddr.test( host ) ) {
-			console.error( `${host} is no IPv4` );
-			continue;
-		}
-		console.log( "Start publishing ping to " + host );
-		pubPing( host );
 	}
 
 
